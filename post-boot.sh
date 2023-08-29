6e67395ff5643697a4b04ba4ed68b42a0d6f7c0c
Suranga Mahesh <72818884+surangamh@users.noreply.github.com>
2023-08-28 21:29:03 -0400

    Update post-boot.sh

--- post-boot.sh
+++ post-boot.sh
@@ -90,17 +90,10 @@ verify_install() {
 # Get the serialized parameters from the command-line argument
 serialized_params="$1"
 
-# Print a message
-echo "Received serialized parameters: $serialized_params"
-
-# Parse the JSON and print individual parameters
-# Assumes `jq` is available for JSON parsing
-param1=$(echo "$serialized_params" | jq -r '.numRAM')
-param2=$(echo "$serialized_params" | jq -r '.numCPU')
 
 # Print individual parameters
-echo "RAM: $param1"
-echo "CPU: $param2"
+echo "paramvalues: $serialized_params"
+
 
 SHELL=1
 OSVERSION=`grep '^ID=' /etc/os-release | awk -F= '{print $2}'`
